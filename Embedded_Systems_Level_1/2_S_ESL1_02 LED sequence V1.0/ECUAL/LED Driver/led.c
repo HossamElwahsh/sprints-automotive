e64127e4887ec66e3ed71cc69d870aedb5cc031a
Hossam Elwahsh <hacker2894@gmail.com>
2023-04-08 04:45:08 +0200

    update: v1 readme

--- Embedded_Systems_Level_1/2_S_ESL1_02 LED sequence V1.0/ECUAL/LED Driver/led.c
+++ Embedded_Systems_Level_1/2_S_ESL1_02 LED sequence V1.0/ECUAL/LED Driver/led.c
@@ -6,25 +6,41 @@
  */
 #include "led.h"
 
-/// Init LED
+/**
+ * Initializes LED on given port & pin
+ * @param ledPort [in] LED Port
+ * @param ledPin [in] LED Pin number in ledPort
+ */
 EN_LED_ERROR_t LED_init(EN_DIO_PORT_T ledPort, uint8_t ledPin) {
     EN_DIO_Error_T dioError = DIO_init(ledPin, ledPort, OUT);
     return dioError == DIO_Error ? LED_ERROR : LED_OK;
 }
 
-/// Turn LED on
+/**
+ * Turns on LED at given port/pin
+ * @param ledPort [in] LED Port
+ * @param ledPin [in] LED Pin number in ledPort
+ */
 EN_LED_ERROR_t LED_on(EN_DIO_PORT_T ledPort, uint8_t ledPin) {
     EN_DIO_Error_T dioError = DIO_write(ledPin, ledPort, HIGH);
     return dioError == DIO_Error ? LED_ERROR : LED_OK;
 }
 
-/// Turn LED off
+/**
+ * Turns off LED at given port/pin
+ * @param ledPort [in] LED Port
+ * @param ledPin [in] LED Pin number in ledPort
+ */
 EN_LED_ERROR_t LED_off(EN_DIO_PORT_T ledPort, uint8_t ledPin) {
     EN_DIO_Error_T dioError = DIO_write(ledPin, ledPort, LOW);
     return dioError == DIO_Error ? LED_ERROR : LED_OK;
 }
 
-/// Toggle LED
+/**
+ * Toggles LED at given port/pin
+ * @param ledPort [in] LED Port
+ * @param ledPin [in] LED Pin number in ledPort
+ */
 EN_LED_ERROR_t LED_toggle(EN_DIO_PORT_T ledPort, uint8_t ledPin) {
     EN_DIO_Error_T dioError = DIO_toggle(ledPin, ledPort);
     return dioError == DIO_Error ? LED_ERROR : LED_OK;
